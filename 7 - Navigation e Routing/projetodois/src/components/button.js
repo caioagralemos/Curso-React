e32254d72766ad72d8100337078b1bbda4145ab1
caioagralemos <caioagralemos@live.com>
2023-05-16 15:58:29 -0300

    7 - Navigation e Routing

--- 7 - Navigation e Routing/projetodois/src/components/button.js
+++ 7 - Navigation e Routing/projetodois/src/components/button.js
@@ -1,29 +1,31 @@
 import PropTypes from 'prop-types'
 
 function Botao({ children = "Ok",
-    primary, 
+    primary,
     secondary,
     success,
     warning,
     danger,
-    outline, 
+    outline,
     rounded }) {
 
     return (
-    <button>
-        {children}
-    </button>
+        <div>
+            <button className='bg-blue-500 text-white border-2 border-blue-600 px-3 py-1.5 m-3'>
+                {children}
+            </button>
+        </div>
     )
 }
 
 
 Botao.propTypes = {
-    checkVariation: ({primary, secondary, success, warning, danger}) => {
+    checkVariation: ({ primary, secondary, success, warning, danger }) => {
         const count = Number(!!primary) // !!algo transforma true em 1 e undefined ou false em 0
-        + Number(!!secondary)
-        + Number(!!success)
-        + Number(!!warning)
-        + Number(!!danger)
+            + Number(!!secondary)
+            + Number(!!success)
+            + Number(!!warning)
+            + Number(!!danger)
 
         if (count > 1) {
             throw new Error('Você escolheu mais de uma classe para seu Botão!')
