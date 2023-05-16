e32e8fc4663c16b3e95bf0a93a18be22db56d657
caioagralemos <caioagralemos@live.com>
2023-05-16 17:17:13 -0300

    7

--- 7 - Navigation e Routing/projetodois/src/App.js
+++ 7 - Navigation e Routing/projetodois/src/App.js
@@ -1,10 +1,45 @@
 import Botao from "./components/button"
+import {GoAlert} from 'react-icons/go'
 
 function App() {
     return (
         <div className="App">
-            <Botao>Pix agora 1500</Botao>
-            <Botao primary secondary />
+            <div>
+                <Botao>Plain</Botao>
+                <Botao rounded>Plain</Botao>
+                <Botao outline>Plain</Botao>
+                <Botao outline rounded>Plain</Botao>
+            </div>
+            <div>
+                <Botao primary>Primary</Botao>
+                <Botao primary rounded>Primary</Botao>
+                <Botao primary outline>Primary</Botao>
+                <Botao primary outline rounded>Primary</Botao>
+            </div>
+            <div>
+                <Botao secondary>Secondary</Botao>
+                <Botao secondary rounded>Secondary</Botao>
+                <Botao secondary outline>Secondary</Botao>
+                <Botao secondary outline rounded>Secondary</Botao>
+            </div>
+            <div>
+                <Botao success>Success</Botao>
+                <Botao success rounded>Success</Botao>
+                <Botao success outline>Success</Botao>
+                <Botao success outline rounded>Success</Botao>
+            </div>
+            <div>
+                <Botao warning><GoAlert /> Warning</Botao>
+                <Botao warning rounded><GoAlert /> Warning</Botao>
+                <Botao warning outline ><GoAlert /> Warning</Botao>
+                <Botao warning outline rounded><GoAlert /> Warning</Botao>
+            </div>
+            <div>
+                <Botao danger>Danger</Botao>
+                <Botao danger rounded>Danger</Botao>
+                <Botao danger outline>Danger</Botao>
+                <Botao danger outline rounded>Danger</Botao>
+            </div>
         </div>
     )
 }
--- 7 - Navigation e Routing/projetodois/src/components/button.js
+++ 7 - Navigation e Routing/projetodois/src/components/button.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types'
+import classnames from 'classnames'
 
 function Botao({ children = "Ok",
     primary,
@@ -9,12 +10,28 @@ function Botao({ children = "Ok",
     outline,
     rounded }) {
 
+    const finalClassName = classnames('flex', 'itens-center','border', 'px-3', 'py-1.5', 'm-3', {
+        'bg-white': outline,
+        'text-black': outline || (secondary && outline),
+        'bg-blue-200': primary,
+        'border-blue-300': primary,
+        'text-white': secondary && !outline,
+        'bg-black': secondary,
+        'border-gray-900': secondary,
+        'bg-green-500': success,
+        'border-green-600': success,
+        'bg-yellow-500': warning && !outline,
+        'border-yellow-600': warning,
+        'bg-red-500': danger,
+        'border-red-600': danger,
+        'rounded-3xl': rounded,
+        'border-black': !primary && !secondary && !success && !warning && !danger
+    })
+
     return (
-        <div>
-            <button className='bg-blue-500 text-white border-2 border-blue-600 px-3 py-1.5 m-3'>
+            <button className={finalClassName}>
                 {children}
             </button>
-        </div>
     )
 }
 
