fdfae8b09f1cdf810d689358231059e5c79b9951
caioagralemos <caioagralemos@live.com>
2023-05-17 20:13:04 -0300

    7 e 8

---
+++ 8 - Pages/projetodois/src/App.js
@@ -0,0 +1,50 @@
+import Botao from "./components/button"
+import {GoAlert} from 'react-icons/go'
+
+function App() {
+    const handleClick = () => {
+        console.log('clicked!')
+    }
+    return (
+        <div className="App">
+            <div>
+                <Botao onClick={handleClick}>Plain</Botao>
+                <Botao rounded onMouseEnter={handleClick}>Plain</Botao>
+                <Botao outline>Plain</Botao>
+                <Botao outline rounded>Plain</Botao>
+            </div>
+            <div>
+                <Botao primary>Primary</Botao>
+                <Botao primary rounded>Primary</Botao>
+                <Botao primary outline>Primary</Botao>
+                <Botao primary outline rounded>Primary</Botao>
+            </div>
+            <div>
+                <Botao secondary className='mb-5'>Secondary</Botao>
+                <Botao secondary rounded>Secondary</Botao>
+                <Botao secondary outline>Secondary</Botao>
+                <Botao secondary outline rounded>Secondary</Botao>
+            </div>
+            <div>
+                <Botao success>Success</Botao>
+                <Botao success rounded>Success</Botao>
+                <Botao success outline>Success</Botao>
+                <Botao success outline rounded>Success</Botao>
+            </div>
+            <div>
+                <Botao warning><GoAlert /> Warning</Botao>
+                <Botao warning rounded><GoAlert /> Warning</Botao>
+                <Botao warning outline ><GoAlert /> Warning</Botao>
+                <Botao warning outline rounded><GoAlert /> Warning</Botao>
+            </div>
+            <div>
+                <Botao danger>Danger</Botao>
+                <Botao danger rounded>Danger</Botao>
+                <Botao danger outline>Danger</Botao>
+                <Botao danger outline rounded>Danger</Botao>
+            </div>
+        </div>
+    )
+}
+
+export default App
---
+++ 8 - Pages/projetodois/src/components/button.js
@@ -0,0 +1,56 @@
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+
+function Botao({ children = "Ok",
+    primary,
+    secondary,
+    success,
+    warning,
+    danger,
+    outline,
+    rounded,
+    className,
+    ...rest }) {
+
+    const finalClassName = classnames('flex', 'itens-center', 'border', 'px-3', 'py-1.5', 'm-3', {
+        'bg-white': outline,
+        'text-black': outline || (secondary && outline),
+        'bg-blue-200': primary,
+        'border-blue-300': primary,
+        'text-white': secondary && !outline,
+        'bg-black': secondary,
+        'border-gray-900': secondary,
+        'bg-green-500': success,
+        'border-green-600': success,
+        'bg-yellow-500': warning && !outline,
+        'border-yellow-600': warning,
+        'bg-red-500': danger,
+        'border-red-600': danger,
+        'rounded-3xl': rounded,
+        'border-black': !primary && !secondary && !success && !warning && !danger
+    }, className)
+
+    return (
+        <button className={finalClassName} {...rest}>
+            {/* onClick={eventoClique} onMouseOver={eventoMouseOver} */}
+            {children}
+        </button>
+    )
+}
+
+
+Botao.propTypes = {
+    checkVariation: ({ primary, secondary, success, warning, danger }) => {
+        const count = Number(!!primary) // !!algo transforma true em 1 e undefined ou false em 0
+            + Number(!!secondary)
+            + Number(!!success)
+            + Number(!!warning)
+            + Number(!!danger)
+
+        if (count > 1) {
+            throw new Error('Você escolheu mais de uma classe para seu Botão!')
+        }
+    }
+}
+
+export default Botao
