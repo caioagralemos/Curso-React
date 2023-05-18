881911750af1918896e674cadf4d049ab785a41e
caioagralemos <caioagralemos@live.com>
2023-05-17 22:36:24 -0300

    ok

--- 7 - Componentes/projetodois/src/components/accordion.js
+++ 7 - Componentes/projetodois/src/components/accordion.js
@@ -2,9 +2,13 @@ import { useState } from "react"
 import { GoArrowLeft, GoArrowDown } from "react-icons/go";
 
 function Accordion({ itens }){
-    const [expandedIndex, setIndex] = useState(0)
+    const [expandedIndex, setIndex] = useState(-1)
     const handleClick = (i) => {
-        setIndex(i)
+        if(expandedIndex === i){
+            setIndex(-1)
+        } else {
+            setIndex(i)
+        }
         // setIndex(index) como se o index do outro tivesse aqui também é permitido se não usarmos arrow func lá embaixo
     }
     const renderedItens = itens.map((item, index) => {
@@ -12,12 +16,12 @@ function Accordion({ itens }){
         let icon = <span>{isExpanded ? <GoArrowDown /> : <GoArrowLeft />}</span>
         return(
             <div key={item.id}>
-                <h5 onClick={() => handleClick(index)}>{item.label}{icon}</h5>
-                {isExpanded && <div>{item.content}</div>}
+                <h5 className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={() => handleClick(index)}>{item.label}{icon}</h5>
+                {isExpanded && <div className="border-b p-5">{item.content}</div>}
             </div>
         )
     })
-    return <div>{renderedItens}</div>
+    return <div className="border-x border-t rounded">{renderedItens}</div>
 }
 
 export default Accordion
