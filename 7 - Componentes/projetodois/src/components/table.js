e0cf9d9fc2c32f2653acf0543d614d81cdcb6ee9
caioagralemos <caioagralemos@live.com>
2023-05-19 15:29:12 -0300

    7 - Table

---
+++ 7 - Componentes/projetodois/src/components/table.js
@@ -0,0 +1,4 @@
+function Table() {
+    return <div>Future Data Table</div>
+}
+export default Table
---
+++ 7 - Componentes/projetodois/src/pages/TablePage.js
@@ -0,0 +1,6 @@
+import Table from "../components/table";
+
+function TablePage() {
+    return <Table />
+}
+export default TablePage
