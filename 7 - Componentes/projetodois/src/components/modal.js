805855272c47de2d77a9756e4a31923a17b3f29e
caioagralemos <caioagralemos@live.com>
2023-05-19 15:19:44 -0300

    7 - Modal

--- 7 - Componentes/projetodois/src/components/modal.js
+++ 7 - Componentes/projetodois/src/components/modal.js
@@ -1,12 +1,27 @@
 import Botao from "./button"
-function Modal() {
-    return (
+import ReactDOM from "react-dom"
+import { useEffect } from "react"
+
+function Modal({ handleClick, children = "I'm a modal", actionBar }) {
+    useEffect(() => {
+        document.body.classList.add('overflow-hidden')
+        return () => {
+            document.body.classList.remove('overflow-hidden')
+        }
+    }, [])
+    return ReactDOM.createPortal( // necessário jogar numa div isolada no html pra que ela sempre possa cubrir a pag inteira
         <div>
-        <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
-        <div className="absolute inset-40 p-10 bg-white">
-            I'm a modal!
-        </div>
-        </div>
+            <div onClick={handleClick} className="fixed inset-0 bg-gray-300 opacity-60"></div>
+            <div className="fixed inset-40 p-10 bg-white border-black border-2 rounded-xl">
+                <div className="flex flex-col justify-between h-full">
+                    {children}
+                    <div className="flex justify-end">
+                        {actionBar}
+                    </div>
+                </div>
+            </div>
+        </div>,
+        document.querySelector('.modal-container')
     )
 }
 export default Modal
--- 7 - Componentes/projetodois/src/pages/ModalPage.js
+++ 7 - Componentes/projetodois/src/pages/ModalPage.js
@@ -2,16 +2,21 @@ import Modal from "../components/modal";
 import Botao from "../components/button";
 import { useState } from "react";
 
-function ModalPage () {
+function ModalPage() {
     const [modalState, setmodalState] = useState(false)
     const handleClick = () => {
-        setmodalState(true)
+        setmodalState(!modalState)
     }
-    return(
+    const actionBar = (
         <div>
-        <Botao onClick={handleClick} primary outline>Modal Button</Botao>
-        {modalState && <Modal />}
+            <Botao primary onClick={handleClick}>Fechar!</Botao>
         </div>
-    ) 
+    )
+    return (
+        <div>
+            <Botao onClick={handleClick} primary outline>Modal Button</Botao>
+            {modalState && <Modal actionBar={actionBar} handleClick={handleClick}>Esse modal é feito pra testar as tecnicas de modal em React e pode ser fechado clicando no botão ou em qualquer lugar fora do modal.</Modal>}
+        </div>
+    )
 }
 export default ModalPage
