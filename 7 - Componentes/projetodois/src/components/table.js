8bf3e750488695286ca5ae8cf0218cb3102114e7
caioagralemos <caioagralemos@live.com>
2023-05-20 19:02:30 -0300

    7 - Table

--- 7 - Componentes/projetodois/src/components/table.js
+++ 7 - Componentes/projetodois/src/components/table.js
@@ -1,4 +1,28 @@
-function Table() {
-    return <div>Future Data Table</div>
+import classNames from "classnames"
+function Table({data}) {
+    const times = data.map((time, index) => {
+        const classe = classNames(time.color, 'border', 'border-black', 'p-3', 'm-2')
+        return (
+            <tr className="border-b" key={time.name}>
+                <td className="p-3">{time.name}</td>
+                <td className='p-3'><div className={classe}></div></td>
+                <td className="p-3">{time.score}</td>
+            </tr>
+        )
+    })
+    return(
+        <table className="table-auto border-spacing-2">
+            <thead>
+                <tr className="border-b-2">
+                    <th>Times</th>
+                    <th>Cor</th>
+                    <th>Pontos</th>
+                </tr>
+            </thead>
+            <tbody>
+                {times}
+            </tbody>
+        </table>
+    )
 }
 export default Table
--- 7 - Componentes/projetodois/src/pages/TablePage.js
+++ 7 - Componentes/projetodois/src/pages/TablePage.js
@@ -1,6 +1,12 @@
 import Table from "../components/table";
 
 function TablePage() {
-    return <Table />
+    const data = [
+        {name: 'Brasil', color: 'bg-yellow-500', score: 6},
+        {name: 'Camarões', color: 'bg-lime-950', score: 4},
+        {name: 'Sérvia', color: 'bg-white', score: 1},
+        {name: 'Suíça', color: 'bg-red-500', score: 5}
+    ]
+    return <Table data={data} />
 }
 export default TablePage
